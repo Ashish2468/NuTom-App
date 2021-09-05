ffe145be058b3867077c768eb7bd4dd791005466
Ashish <73551845+Ashish2468@users.noreply.github.com>
2021-09-05 12:34:07 +0530

    removed homescreen js

--- App.js
+++ App.js
@@ -9,16 +9,7 @@ import Form from './screens/Form';
 
 enableScreens();
 
-// function HomeScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button
-//         title="Go to Profile"
-//         onPress={() => navigation.navigate('Profile')}
-//       />
-//     </View>
-//   );
-// }
+
 
 function ProfileScreen({ navigation }) {
   return (
